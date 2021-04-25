aa226fc03882dff0ad4cf6ee9d3d983e93e8fc6a
Blue <13604413+Blumlaut@users.noreply.github.com>
2021-04-25 17:14:08 +0200

    Revert "twitter db stuff"
    This reverts commit 2b59ae76cc79bcbf7e0934d8c80badd1ede63f59.

--- esxfull12.sql
+++ esxfull12.sql
@@ -555,39 +555,6 @@ CREATE TABLE IF NOT EXISTS `society_moneywash` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-DROP TABLE IF EXISTS `twitter_accounts`;
-CREATE TABLE IF NOT EXISTS `twitter_accounts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) CHARACTER SET utf8 NOT NULL DEFAULT '0',
-  `password` varchar(64) COLLATE utf8mb4_bin NOT NULL DEFAULT '0',
-  `avatar_url` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
-DROP TABLE IF EXISTS `twitter_likes`;
-CREATE TABLE IF NOT EXISTS `twitter_likes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `authorId` int(11) DEFAULT NULL,
-  `tweetId` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_twitter_likes_twitter_accounts` (`authorId`),
-  KEY `FK_twitter_likes_twitter_tweets` (`tweetId`)
-) ENGINE=InnoDB AUTO_INCREMENT=137 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
-DROP TABLE IF EXISTS `twitter_tweets`;
-CREATE TABLE IF NOT EXISTS `twitter_tweets` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `authorId` int(11) NOT NULL,
-  `realUser` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `message` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `time` timestamp NOT NULL DEFAULT current_timestamp(),
-  `likes` int(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`),
-  KEY `FK_twitter_tweets_twitter_accounts` (`authorId`)
-) ENGINE=InnoDB AUTO_INCREMENT=170 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `identifier` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -977,16 +944,6 @@ INSERT INTO `weashops` (`id`, `zone`, `item`, `price`) VALUES
 (38, 'BlackWeashop', 'WEAPON_MINIGUN', 45000),
 (39, 'BlackWeashop', 'WEAPON_RAILGUN', 50000),
 (40, 'BlackWeashop', 'WEAPON_STICKYBOMB', 500);
-
-
-ALTER TABLE `twitter_likes`
-  ADD CONSTRAINT `FK_twitter_likes_twitter_accounts` FOREIGN KEY (`authorId`) REFERENCES `twitter_accounts` (`id`),
-  ADD CONSTRAINT `FK_twitter_likes_twitter_tweets` FOREIGN KEY (`tweetId`) REFERENCES `twitter_tweets` (`id`) ON DELETE CASCADE;
-
-ALTER TABLE `twitter_tweets`
-  ADD CONSTRAINT `FK_twitter_tweets_twitter_accounts` FOREIGN KEY (`authorId`) REFERENCES `twitter_accounts` (`id`);
-COMMIT;
-
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
