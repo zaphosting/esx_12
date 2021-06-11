5e64ae9f2e8070cb5acaf18a70343426cd71036b
Blue <13604413+Bluethefurry@users.noreply.github.com>
2021-06-11 12:04:16 +0200

    Update es.lua

--- esx_bankerjob/locales/es.lua
+++ esx_bankerjob/locales/es.lua
@@ -10,8 +10,9 @@ Locales['es'] = {
   ['deposit']                           = 'depositar',
   ['invalid_amount']                    = 'Monto invalido',
   ['no_player_nearby']                  = 'no hay jugadores cerca',
-['press_input_context_to_open_menu']    = 'presione ~INPUT_CONTEXT~ para abrir el ~g~Menú del banc ~s~.',
+  ['press_input_context_to_open_menu']    = 'presione ~INPUT_CONTEXT~ para abrir el ~g~Menú del banc ~s~.',
   ['withdraw']                          = 'retirar',
   ['boss_actions']                      = 'Acciones del jefe',
   ['phone_receive']                     = 'Cliente bancario',
   ['phone_label']                       = 'banco',
+}
